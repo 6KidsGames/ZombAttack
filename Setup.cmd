4c4a27753cae1cc5cb41ebf3941f6497a09686cf
erikma <erikma1@hotmail.com>
2016-05-16 01:35:51 -0700

    Enhance gulpfile with extra modules, add to Setup.cmd, and create initial package.json

--- Setup.cmd
+++ Setup.cmd
@@ -9,8 +9,17 @@ echo ==========================================================================
 echo.
 echo Installing Gulp ^(http://gulpjs.com/^) command line
 echo We use this as a better way to build code than trying to use Windows command scripts.
+echo.
 call npm install --global gulp-cli
 if ERRORLEVEL 1 echo ERROR: npm install --global gulp-cli failed with errorlevel %ERRORLEVEL% && exit /b 1
 call npm install --save-dev gulp
 if ERRORLEVEL 1 echo ERROR: npm install --save-dev gulp failed with errorlevel %ERRORLEVEL% && exit /b 1
 
+echo.
+echo Installing Gulp modules we need:
+echo   Uglify - for minifying JavaScript
+echo   pump - simplifies dealing with Node.js streams when lots of piping is used.
+echo   del - deletes files and folders
+echo   path - Always better than using string concatenation.
+echo.
+call npm install --save-dev gulp-uglify pump del path
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,46 @@
 //
-// Root Gulp file for building the class code.
+// Root Gulp file for building the project code.
 // Gulp home is at http://gulpjs.com/ 
 //
 
+// Base Gulp library.
 var gulp = require('gulp');
 
+// Use the path class for path joining.
+var path = require('path');
+
+// Uglify minifies JavaScript in our project to reduce download times for browsers browsing web sites.
+// https://github.com/terinjokes/gulp-uglify
+var uglify = require('gulp-uglify');
+
+// pump makes it easier to debug chains of Node.js streams.
+// https://github.com/mafintosh/pump
+var pump = require('pump');
+
+// del allows cleaning up folders and files. 
+const del = require('del');
+
+// Keep important paths here for reference. Only use Paths.Xxx in code below instead of duplicating these strings.
+Paths = {
+    Site: 'site/',
+    OutputRoot: 'out/',
+    SiteOutput: 'out/site/'
+};
+
 gulp.task('default', function() {
-    // place code for your default task here
+    gulp.run('compress');
+});
+
+gulp.task('clean', function (done) {
+    // Clean up temp and output directories.
+    del([Paths.OutputRoot]);
+});
+
+gulp.task('compress', function (cb) {
+    pump([
+            gulp.src(Paths.Site + 'scripts/**/*.js'),
+            uglify(),
+            gulp.dest(path.join(Paths.SiteOutput, 'scripts'))
+        ],
+        cb);
 });
