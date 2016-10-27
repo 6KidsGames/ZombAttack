686966c009fb26c134f006c8f981aa1236fba6ff
erikma <erikma1@hotmail.com>
2016-10-26 22:56:01 -0700

    Deploy PPE

---
+++ Deployed/PPE/site/Player.js
@@ -0,0 +1,177 @@
+// Player-related data and code.
+
+const Level = require('./Level');
+const Log = require('./Log');
+const Physics = require('./Physics');
+const Util = require('./Util');
+const Weapon = require('./Weapon');
+
+const playerMaxTurnPerFrameRadians = 0.3;
+const playerSpeedPxPerFrame = 10;
+const playerOuchSoundsPerCharacter = 2;
+
+// Returns a new PlayerInfo object. Called at connection of the client, before
+// we have any identifying information.
+// The PlayerInfo data structure is the full server-side view of the player.
+// The 'player' object inside iof it is the information that is shared with clients.
+// Player property names are deliberately kept short to reduce space over the network.
+function spawnPlayer(spark, currentLevel) {
+  let x = Util.getRandomInt(32, currentLevel.widthPx - 32);
+  let y = Util.getRandomInt(32, currentLevel.heightPx - 32);
+
+  const defaultWeaponIndex = 0;
+  let defaultWeaponType = Weapon.WeaponTypes[defaultWeaponIndex]; 
+
+  let weaponTracker = {
+    weaponType: defaultWeaponType,
+    currentAmmo: defaultWeaponType.ammo,
+  };
+
+  return {
+    spark: spark,
+
+    latestControlInfo: { },
+
+    // The abstract representation of the player for hit detection purposes.
+    modelCircle: Physics.circle(x, y, 16),
+
+    currentWeapon: weaponTracker,
+    lastWeaponUse: 0,  // allow to use weapon immediately
+    inventory: [ weaponTracker ],
+
+    lastWeaponChangeID: 0,  // Track client weapon change keypresses
+    
+    player: {
+      id: spark.id,  // Used by clients to self-identify
+
+      // Place the player in a random location on the map.
+      // TODO: Account for the contents of the underlying tile - only place users into locations that
+      // make sense, or at map-specific spawn points.
+      x: x,
+      y: y,
+      dir: 0.0,
+      inv: [ defaultWeaponIndex ],  // Inventory
+      w: defaultWeaponIndex,  // Current weapon number
+      hl: 10,  // health
+      snd: 0,  // sound number
+      sndC: 0,  // sound state machine
+      wuse: 0,  // Weapon ID in use
+      wC: 0,  // Weapon use state machine - increments on actual weapon use. Used for triggering sounds and other actions.
+      // 'dead' variable starts undefined and gets set true on player death.
+    }
+  };
+}
+
+function updatePlayerFromClientControls(playerInfo, currentLevel) {
+  let player = playerInfo.player;
+  if (player.dead) {
+    // Dead, can't do much of anything but bleed out.
+    return;
+  }
+  
+  let controlInfo = playerInfo.latestControlInfo;
+  if (controlInfo.R) {  // Right
+    player.dir += playerMaxTurnPerFrameRadians;
+  }
+  if (controlInfo.L) {  // Left
+    player.dir -= playerMaxTurnPerFrameRadians;
+  }
+  if (controlInfo.F) {  // Forward
+    player.x += playerSpeedPxPerFrame * Math.sin(player.dir);
+    player.y -= playerSpeedPxPerFrame * Math.cos(player.dir);
+    Level.clampPositionToLevel(currentLevel, player);
+    playerInfo.modelCircle.centerX = player.x;
+    playerInfo.modelCircle.centerY = player.y;
+  }
+  if (controlInfo.B) {  // Back
+    player.x -= playerSpeedPxPerFrame * Math.sin(player.dir);
+    player.y += playerSpeedPxPerFrame * Math.cos(player.dir);
+    Level.clampPositionToLevel(currentLevel, player);
+    playerInfo.modelCircle.centerX = player.x;
+    playerInfo.modelCircle.centerY = player.y;
+  }
+  if (controlInfo.wC > playerInfo.lastWeaponChangeID) {
+    playerInfo.lastWeaponChangeID = controlInfo.wC;
+    let weaponID = Util.clamp(controlInfo.w, 0, Weapon.NumWeapons - 1); 
+    //Log.debug("Weapon change", controlInfo.w);
+    if (player.w !== weaponID) {
+      let weaponTracker = playerInfo.inventory.find(tracker => tracker.weaponType.number === weaponID);
+      if (weaponTracker) {
+        //Log.debug("Found weapon in inventory, changing");
+        player.w = weaponID;
+        playerInfo.currentWeapon = weaponTracker;
+      }
+    }
+  }
+}
+
+// Called at the end of player processing.
+function updatePlayer(playerInfo, currentTime) {
+  playerInfo.player.a = playerInfo.currentWeapon.currentAmmo;
+}
+
+function hitByZombie(playerInfo, currentTime) {
+  let player = playerInfo.player;
+  player.hl -= 1;
+  if (player.hl <= 0) {
+    playerInfo.deadAt = currentTime;
+    player.dead = true;
+    player.snd = 0;  // Last death sound.
+    player.sndC++;
+  } else {
+    // Pick a hurt sound to play.
+    player.snd = Util.getRandomInt(0, playerOuchSoundsPerCharacter);
+    player.sndC++;
+  }
+}
+
+// Called when a player runs over a weapon.
+// Returns true if the player really picked up the weapon (player might not need it if already in inventory).
+function pickedUpWeapon(playerInfo, weaponInfo, currentTime) {
+  let newWeaponType = weaponInfo.type;
+  let player = playerInfo.player;
+  let existingWeaponTracker = playerInfo.inventory.find(t => t.weaponType === newWeaponType); 
+  if (existingWeaponTracker) {
+    if (newWeaponType.type === "Melee") {
+      // When we already have a melee weapon we don't pick up another one.
+      return false;
+    }
+    
+    // Take the ammo from the ranged weapon.
+    existingWeaponTracker.currentAmmo += newWeaponType.ammo;
+    return true;
+  }
+
+  let weaponTracker = {
+    weaponType: newWeaponType,
+    currentAmmo: newWeaponType.ammo
+  };
+  playerInfo.inventory.push(weaponTracker);
+  player.inv.push(newWeaponType.number);
+
+  if (playerInfo.currentWeapon.weaponType.awesomeness < newWeaponType.awesomeness) {
+    playerInfo.currentWeapon = weaponTracker;
+    player.w = newWeaponType.number;
+  }
+  return true;
+}
+
+function dropWeapon(playerInfo, weaponTracker) {
+  playerInfo.inventory.remove(weaponTracker);
+  playerInfo.player.inv.remove(weaponTracker.weaponType.number);
+  if (playerInfo.currentWeapon.weaponType.number === weaponTracker.weaponType.number) {
+    playerInfo.inventory.sort((a, b) => b.weaponType.awesomeness - a.weaponType.awesomeness);
+    playerInfo.currentWeapon = playerInfo.inventory[0];
+    playerInfo.player.w = playerInfo.inventory[0].weaponType.number;
+  }
+}
+
+
+// --------------------------------------------------------------------
+// Exports
+module.exports.spawnPlayer = spawnPlayer;
+module.exports.updatePlayer = updatePlayer;
+module.exports.updatePlayerFromClientControls = updatePlayerFromClientControls;
+module.exports.hitByZombie = hitByZombie;
+module.exports.pickedUpWeapon = pickedUpWeapon;
+module.exports.dropWeapon = dropWeapon;
---
+++ Deployed/PPE/site/Weapon.js
@@ -0,0 +1,110 @@
+// Weapon-related code.
+
+const Util = require('./Util');
+const Log = require('./Log');
+const Physics = require('./Physics');
+
+// If the zombie and the player model circles are just touching, the centers are 32 pixels distance
+// from each other. So the bare-minimum distance for a melee weapon is this far,
+// then we add in any extra small range we want to allow based on the length of the weapon.
+const minMeleeStrikeDistance = 16 + 16;
+
+const weaponLifeMsec = 30 * 1000;
+
+// Map/dictionary of weapons in the game, by name.
+// Ammo value -1 means infinite, anything less means after ammo reaches zero the player drops the empty weapon.
+const WeaponTypes = [
+  // Melee weapons
+  { name: "Dagger", number: 0, awesomeness: 0, probability: 0, type: "Melee", damage: 2, rechargeMsec: 500, rangePx: minMeleeStrikeDistance + 8, ammo: -1 },
+  { name: "HalliganTool", number: 1, awesomeness: 10, probability: 10, type: "Melee", damage: 4, rechargeMsec: 1000, rangePx: minMeleeStrikeDistance + 16, ammo: -1 },
+  { name: "Sword", number: 2, awesomeness: 20, probability: 5, type: "Melee", damage: 5, rechargeMsec: 750, rangePx: minMeleeStrikeDistance + 16, ammo: -1 },
+  { name: "Chainsaw", number: 3, awesomeness: 30, probability: 8, type: "Melee", damage: 1, rechargeMsec: 100, rangePx: minMeleeStrikeDistance + 8, ammo: -1 },
+
+  // Ranged weapons
+  { name: "Pistol", number: 4, awesomeness: 50, probability: 15, type: "Range", damage: 3, rechargeMsec: 500, accuracyConeRad: 0.4, rangePx: 192, ammo: 15 },
+  { name: "Rifle", number: 5, awesomeness: 60, probability: 8, type: "Range", damage: 12, rechargeMsec: 800, accuracyConeRad: 0.2, rangePx: 512, ammo: 12 },
+  { name: "MachineGun", number: 6, awesomeness: 70, probability: 2, type: "Range", damage: 10, rechargeMsec: 100, accuracyConeRad: 0.3, rangePx: 384, ammo: 30 },
+  { name: "Minigun", number: 7, awesomeness: 80, probability: 1, type: "Range", damage: 12, rechargeMsec: 10, accuracyConeRad: 0.3, rangePx: 324, ammo: 200 },
+];
+const NumWeapons = 8;
+
+function getWeaponStats(weaponName) {
+  return WeaponTypes.find(w => w.name === weaponName);
+}
+
+// Creates a map from a number in the range of 0..totalZombieProbability to the zombie type
+// to use if that number is chosen randomly.
+let totalWeaponProbability = 0;
+function createWeaponProbabilityNap() {
+  let probMap = { };
+  let currentProb = 0;
+  WeaponTypes.forEach(weaponType => {
+    totalWeaponProbability += weaponType.probability;
+    for (let i = 0; i < weaponType.probability; i++) {
+      probMap[currentProb] = weaponType;
+      currentProb++;
+    }
+  });
+  return probMap;
+}
+const weaponProbabilityMap = createWeaponProbabilityNap();
+
+let nextWeaponNumber = 0;
+
+function spawnWeapon(level, currentTime) {
+  let weaponID = nextWeaponNumber;
+  nextWeaponNumber++;
+
+  let x = Util.getRandomInt(32, level.widthPx - 32);
+  let y = Util.getRandomInt(32, level.heightPx - 32);
+
+  let randomWeaponNumber = Util.getRandomInt(0, totalWeaponProbability);
+  let weaponType = weaponProbabilityMap[randomWeaponNumber];
+
+  Log.debug(`Creating weapon ${weaponType.name}, number ${weaponType.number}`);
+
+  // A WeaponInfo is the server-side data structure containing all needed server tracking information.
+  // Only a subset of this information is passed to the clients, to minimize wire traffic.
+  let weaponInfo = {
+    modelCircle: Physics.circle(x, y, 24),
+    
+    type: weaponType,
+    timeOutAt: currentTime + weaponLifeMsec,
+
+    // The portion of the data structure we send to the clients.
+    weapon: {
+      id: weaponID,
+
+      // Place the weapon in a random location on the map.
+      // TODO: Account for the contents of the underlying tile - only place weapons into locations that
+      // make sense, or at map-specific spawn points.
+      x: x,
+      y: y,
+      n: weaponType.number,
+    }
+  };
+
+  return weaponInfo;
+}
+
+function isTimedOut(weaponInfo, currentTime) {
+  return (currentTime >= weaponInfo.timeOutAt);
+}
+
+function isPickedUp(weaponInfo, playerInfo) {
+  if (Physics.hitTestCircles(playerInfo.modelCircle, weaponInfo.modelCircle)) {
+    Log.debug(`W${weaponInfo.weapon.id}: Picked up by ${playerInfo.player.id}`);
+    return true;
+  }
+  return false;
+}
+
+
+// --------------------------------------------------------------------
+// Exports
+module.exports.WeaponTypes = WeaponTypes;
+module.exports.NumWeapons = NumWeapons;
+module.exports.spawnWeapon = spawnWeapon;
+module.exports.isTimedOut = isTimedOut;
+module.exports.isPickedUp = isPickedUp;
+module.exports.getWeaponStats = getWeaponStats;
